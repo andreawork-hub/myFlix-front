44ea427086e7b8976a821d8b93a3e67ab0c6fada
andreawork-hub <andrea.vitova@hotmail.com>
2023-01-20 02:12:30 +0100

    Updated main-view.jsx file - import

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,7 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
-import { useState, useEffect } from "react";
 
 export const MainView = () => {
   const [movies, setMovies] = useState([]);
