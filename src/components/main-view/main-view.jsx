c6833aa465808acfe41db4f360156703f8b173d4
andreawork-hub <andrea.vitova@hotmail.com>
2023-01-22 23:33:44 +0100

    Updated main-view.jsx file
    - integrated the login and signup view with MainView
    - implemented the authentication token along with the fetch requests in main-view.jsx to get all movies from the authenticated /movies endpoint on the backend

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,35 +1,74 @@
 import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
   const [movies, setMovies] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(null);
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
 
   useEffect(() => {
-    fetch("https://movie-api-lnmw.onrender.com/movies")
+    if (!token) {
+      return;
+    }
+    fetch("https://movie-api-lnmw.onrender.com/movies", {
+      headers: { Authorization: `Bearer ${token}` },
+    })
       .then((response) => response.json())
-      .then((data) => {
-        const moviesFromApi = data.docs.map((doc) => {
-          return {
-            id: doc.key,
-            title: doc.title,
-            image: `https://covers.openlibrary.org/b/id/${doc.cover_i}-L.jpg`,
-            director: doc.author_name?.[0],
-          };
+      .then((movies) => {
+        console.log(movies);
+        setMovies(movies);
+      });
+  }, [token]);
+  /*const moviesFromApi = movies.map((movie) => {
+            const { _id, ...rest } = movie;
+            return {
+              ...rest,
+              id: movie._id,
+              };
         });
 
         setMovies(moviesFromApi);
       });
-  }, []);
+  }, []); */
 
-  const [selectedMovie, setSelectedMovie] = useState(null);
+  if (!user) {
+    return (
+      <>
+        <LoginView
+          onLoggedIn={(user, token) => {
+            setUser(user);
+            setToken(token);
+          }}
+        />
+        or
+        <SignupView />
+      </>
+    );
+  }
 
   if (selectedMovie) {
     return (
-      <MovieView
-        movie={selectedMovie}
-        onBackClick={() => setSelectedMovie(null)}
-      />
+      <>
+        <button
+          onClick={() => {
+            setUser(null);
+            setToken(null);
+            localStorage.clear();
+          }}
+        >
+          Logout
+        </button>
+        <MovieView
+          movie={selectedMovie}
+          onBackClick={() => setSelectedMovie(null)}
+        />
+      </>
     );
   }
 
@@ -42,9 +81,7 @@ export const MainView = () => {
         <MovieCard
           key={movie.id}
           movie={movie}
-          onMovieClick={(newSelectedMovie) => {
-            setSelectedMovie(newSelectedMovie);
-          }}
+          onMovieClick={() => setSelectedMovie(movie)}
         />
       ))}
     </div>
